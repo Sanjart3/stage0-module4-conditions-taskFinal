09aaeb3d95ef4510a392d0e2df377f70e9dacdac
stcoderts <115059135+stcoderts@users.noreply.github.com>
2022-12-26 20:23:47 +0500

    just

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        int side;
+        if (x==0&&y==0){
+            System.out.println("zero");
+        }else{
+            if (x>=0){
+                if (y>=0) side=1;
+                else side = 2;
+            }else {
+                if (y>=0) side=3;
+                else side = 4;
+            }
+            System.out.println(side);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        int febday = 28;
+        if (year%4==0&&(year/100)%4==0){
+            febday = 29;
+        }
+        switch (month){
+            case 1,3,5,7,8,10,12:
+                System.out.println(31); break;
+            case 4,6,9,11:
+                System.out.println(30); break;
+            case 2:
+                System.out.println(febday); break;
+            default:
+                System.out.println("invalid date");
+        }
     }
 }
